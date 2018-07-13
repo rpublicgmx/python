9c62a6897a23a2d8b7d4d942b982c7e5583dcd67
russellpoore <russell.poore@myriadgroup.com>
2018-07-13 17:14:14 +0100

    Don't run script when imported

--- meteors/find_meteors.py
+++ meteors/find_meteors.py
@@ -19,18 +19,20 @@ def calc_dist(lat1, lon1, lat2, lon2):
 def get_dist(meteor):
     return meteor.get('distance', math.inf)
 
-my_loc = (50.935122, -1.7902822)
+if __name__ == '__main__':
 
-meteor_resp = requests.get('https://data.nasa.gov/resource/y77d-th95.json')
-meteor_data = meteor_resp.json()
+    my_loc = (50.935122, -1.7902822)
 
-for meteor in meteor_data:
-    if not ('reclat' in meteor and 'reclong' in meteor): continue
-    meteor['distance'] = calc_dist(float(meteor['reclat']),
-                                   float(meteor['reclong']),
-                                   my_loc[0],
-                                   my_loc[1])
+    meteor_resp = requests.get('https://data.nasa.gov/resource/y77d-th95.json')
+    meteor_data = meteor_resp.json()
 
-meteor_data.sort(key=get_dist)
+    for meteor in meteor_data:
+        if not ('reclat' in meteor and 'reclong' in meteor): continue
+        meteor['distance'] = calc_dist(float(meteor['reclat']),
+                                       float(meteor['reclong']),
+                                       my_loc[0],
+                                       my_loc[1])
 
-print(meteor_data[0:1])
+    meteor_data.sort(key=get_dist)
+
+    print(meteor_data[0:1])
